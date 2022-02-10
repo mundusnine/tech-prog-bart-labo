36c919bce3200559af1c1dd98a516a54e92f5648
Maxime Morin <max_morin99@hotmail.com>
2022-02-10 11:57:03 -0500

    Commit labS1J2

---
+++ labS1J2/Sources/labo.c
@@ -0,0 +1,48 @@
+#include "labo.h"
+
+void insertionSort(int elements[], int n)
+{
+	for (int i = 1; i < n; i++)
+	{
+		int y = i;
+		while (y > 0 && elements[y] < elements[y - 1])
+		{
+			int temp = elements[y];
+			elements[y] = elements[y - 1];
+			elements[y - 1] = temp;
+			y--;
+		 }
+	}
+}
+
+void quickSort(int* elements, int left, int right)
+{
+	if (left >= right)
+	{
+		return;
+	}
+	int L = left;
+	int R = right;
+	int pivot = elements[(left + right) / 2];
+	while (left <= right)
+	{
+		while (elements[left] < pivot)
+		{
+			left++;
+		}
+		while (elements[right] > pivot)
+		{
+			right--;
+		}
+		if (left <= right)
+		{
+			elements[left] = elements[right];
+			elements[right] = elements[left];
+			left++;
+			right--;
+		}
+	}
+	int index = left;
+	quickSort(elements, L, index - 1);
+	quickSort(elements, index, R);
+}
