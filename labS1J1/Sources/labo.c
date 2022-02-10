d43a205ce227d82ea0066df0f3d0341fc9edf0f2
Jau <jogag08@gmail.com>
2022-02-09 21:30:50 -0500

    commit

--- labS1J1/Sources/labo.c
+++ labS1J1/Sources/labo.c
@@ -1,7 +1,7 @@
 #include "labo.h"
 
-char* sousTexte(char* texte, int from, int to) {
-	static char sub[100] = { 0 };
+char* sousTexte(char* texte, int from, int to)  {
+	char sub[100] = {0};
 	char* start = &texte[from];
 	int length = abs(to - from);
 
@@ -9,8 +9,8 @@ char* sousTexte(char* texte, int from, int to) {
 	{
 		sub[i] = start[i];
 	}
-	
 	sub[length] = 0;
+	
 	return sub;
 } 
 
@@ -18,7 +18,7 @@ char* sousTexte(char* texte, int from, int to) {
 * Faire une fonction qui copie le contenu du tableau de character dans la propriété de data
 * de la structure string.
 */
-void set_String(char* text, String* s) {
+void set_String(char* text, String* s)  {
 	s->characters = text;
 	s->length = strlen(text);
 }
@@ -28,17 +28,48 @@ void set_String(char* text, String* s) {
 * Faire une fonction qui vérifie si un mot est un palindrome i.e. kayak, racecar...
 * Si le mot est un palindrome retourner 1 sinon 0
 */
-int String_is_palindrome(String* s) {
-	int longueur = strlen(s->characters);
-	char inverse[10] = { 0 };
+int String_is_palindrome(String* s)  {
+	int length = strlen(s->characters);
+	char backwards[100] = { 0 };
+	
+	for (int i = 0; i < length; i++)
+	{
+		backwards[i] = s->characters[length - (i + 1)];
+	}
+	printf("%s\n", backwards);
 
-
-
+	if (strcmp(backwards, s->characters) == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
 
 /*
 * Implémenter la fonction bubble sort et trier le tableau
 */
-void bubbleSort(int elements[], int n) {
+void bubbleSort(int elements[], int n) 
+{
+	int copy = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			printf("%d ", elements[i]);
+		}
 
+		for (int i = 0; i < n - 1; i++)
+		{
+			if (elements[i] > elements[i + 1])
+			{
+				copy = elements[i];
+				elements[i] = elements[i + 1];
+				elements[i + 1] = copy;
+			}
+		}
+		printf("\n");
+	}
 }
