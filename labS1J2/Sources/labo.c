be4e7a45d83d2c3a3ee60c143b15fe62d2fe2f4f
JLH54 <julien_.com@hotmail.com>
2022-02-10 14:43:34 -0500

    Labo S2J1 fini

---
+++ labS1J2/Sources/labo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void insertionSort(int elements[], int n)
+{
+
+}
+
+void quickSort(int* elements, int left, int right)
+{
+
+}
