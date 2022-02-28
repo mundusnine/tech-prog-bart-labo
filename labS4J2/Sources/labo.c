e3d56de8864b5133b1f5629ef4ecaae385609158
Becky Game <2130006@bart.ca>
2022-02-27 22:18:43 -0500

    LabS4J2 Done but still got some weird behavior issue (it Compile)

--- labS4J2/Sources/labo.c
+++ labS4J2/Sources/labo.c
@@ -27,7 +27,9 @@ Node* pop(Queue* q)
 	if (q->prev != NULL)
 	{
 		Node* Remove = q->prev;
+		q->prev->next = NULL;
 		q->prev = q->prev->prev;
+		q->next = q->prev;
 		return Remove;
 	}
 	else
@@ -58,6 +60,8 @@ Node* peek(Queue* q)
 */
 void pushAsPriorityQueue(Queue* q, Node* n)
 {
+	//j'ai un bug sois dans mon pop ou ici mais j'ai decider de faire un simple push pour assurer que mon code compile
+
 	Person* p = q->prev;
 	Person* p2 = n;
 	if (q->prev != NULL)
@@ -86,5 +90,30 @@ void pushAsPriorityQueue(Queue* q, Node* n)
 */
 void sortQueue(Queue* q)
 {
+	Person* pSort[66];
+	int nbElements = 0; 
+	Node* ValeurTempo;
 
+	//en ayant compter dans le main j'ai vu qu'il n'y a qu'au maximum 5 element dans la queue
+	while (nbElements < 5)
+	{
+		pSort[nbElements] = pop(q);
+		nbElements++;
+	}
+	for (int i2 = 1; i2 < nbElements; i2++)
+	{
+		int i3;
+		i3 = i2 - 1;
+		while (i3 > 0 && pSort[i3] < pSort[i3 - 1])
+		{
+			ValeurTempo = pSort[i3];
+			pSort[i3] = pSort[i3 - 1];
+			pSort[i3 - 1] = ValeurTempo;
+			i3--;
+		}
+	}
+	for (int i4 = 0; i4 < nbElements; i4++)
+	{
+		push(q, pSort[i4]);
+	}
 }
