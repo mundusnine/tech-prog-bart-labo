8d7ff3c7793ed31de47c7c60b669e0e194c5642a
Jau <jogag08@gmail.com>
2022-03-01 22:49:31 -0500

    S4J1 done

--- labS4J1/Sources/labo.c
+++ labS4J1/Sources/labo.c
@@ -16,6 +16,10 @@ void push(Stack* s, void* newData)
 		s->top++;
 		s->data[s->top - 1] = newData;
 	}
+	else
+	{
+		return;
+	}
 }
 
 void* pop(Stack* s)
@@ -35,15 +39,68 @@ void* pop(Stack* s)
 
 void* peek(Stack* s)
 {
-	
+	if (s->top == 0)
+	{
+		return NULL;
+	}
+	else
+	{
+		void* PeekData = s->data[s->top - 1];
+		return PeekData;
+	}
 }
 
 void reverseStack(Stack* s)
 {
-
+	int NbrElem = s->top;
+	int j = 0;
+	void** datas = allocate(s->top * sizeof(void*));
+	
+	for(int i = 0; i < NbrElem; i++)
+	{
+		datas[i] = s->data[i];
+		s->data[i] = NULL;
+	}
+	
+	for(int i = (NbrElem - 1); i >= 0; i--)
+	{
+		s->data[j] = datas[i];
+		j++;
+	}
 }
 
 void sortStack(Stack* s)
 {
+	int swap = 0;
+	int NbrElem = s->top;
+	Person* person = s->data[s->top - 1];
+	Person** personTablo = allocate(sizeof(Person) * NbrElem);
+	
+	for(int i = NbrElem - 1; i >= 0; i--)
+	{
+		personTablo[i] = person;
+		pop(s);
+		person = s->data[s->top - 1];
+	}
 
+	for (int i = 0; i < NbrElem - 1; i++)
+	{
+		if (personTablo[i]->age < personTablo[i + 1]->age)
+		{
+			Person* personCopy = personTablo[i];
+			personTablo[i] = personTablo[i + 1];
+			personTablo[i + 1] = personCopy;
+			swap = 1;
+		}
+		if (swap == 1)
+		{
+			swap = 0;
+			i = -1;
+		}
+	}
+
+	for (int i = 0; i < NbrElem; i++)
+	{
+		push(s, personTablo[i]);
+	}
 }
